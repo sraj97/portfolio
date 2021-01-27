845a76af00c914e6a821fb0ae3f8670af29dc3ce
sraj97 <shwetarajput0702@gmail.com>
2021-01-27 10:14:38 +0000

    JS moved into Main file for Random Movie Generator

--- main.js
+++ main.js
@@ -18,5 +18,12 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+function myFunction() {
+  randomButton = document.getElementById("randomButton");
+  let choice = document.getElementById("chosenOne"); 
 
+  const random = ["Lion King", "Incredibles", "Moana" , "Coco", "Finding Nemo", "Inside Out", "Funny Girl","Ocean's 8", "Pride & Prejudice", "Deadpool", "The Terminal", "Jurrasic World", "Hairspray", "The Help", "Blade","Pretty Woman", "The Ugly Truth", "Eat Pray Love", "Leap Year", "Definitely Maybe", "50 First Dates", "Chalet Girl", "Friends with Benefits", "Letters to Juliet", "La La Land", "Hamilton", "The Proposal", "The Intern", "Mr & Mrs Smith", "Sex & the City"];
 
+  choice.innerHTML = random[Math.floor(Math.random()*random.length)];
+
+}
